bfe1473c81cab69455a4e1f0fbf763dc0eed1d37
max <maxime.carpentier62350@gmail.com>
2018-04-20 11:20:58 +0200

    Ajout tableau

--- jeu.h
+++ jeu.h
@@ -1,1 +1,34 @@
-..
+#include <error.h>
+
+int tabValeur[17][17];
+
+void EnregistrerPositionTab(char position,int touche)
+{
+	tabValeur[(int)position[0]][(int)position[1]] = touche;
+} 
+
+void AfficherTab()
+{
+	char ligne;
+	char valeurAff = " ";
+
+	for (i = 0 ; i < 17 ; i++)
+	{
+		ligne = "";
+		for (j = 0 ; j < 17 ; j++)
+        	{
+			if(tabValeur[i][j] == 1)
+			{
+				valeurAff = "x";
+			}
+			else
+			{
+				valeurAff = " ";
+			}
+
+			ligne = ligne + valeurAff + "|";
+		}
+
+		printf("%d/n", ligne);
+	}
+}
